40da56e8b8caf716f93b92836c4c52343ed90af5
Simon Takita <smtakita@gmail.com>
2022-09-19 15:16:44 -0700

    Refactor draw box into function

--- examples/ex3.rs
+++ examples/ex3.rs
@@ -4,63 +4,149 @@ use std::{
     io::{stdout, Write},
 };
 
-fn main() -> Result<()> {
+pub struct EdgeSymbols {
+    t: char,
+    b: char,
+    l: char,
+    r: char,
+    tl: char,
+    tr: char,
+    bl: char,
+    br: char,
+}
+
+impl EdgeSymbols {
+    pub fn default() -> EdgeSymbols {
+        EdgeSymbols {
+            t: '─',
+            b: '─',
+            l: '│',
+            r: '│',
+            tl: '┌',
+            tr: '┐',
+            bl: '└',
+            br: '┘',
+        }
+    }
+}
+
+fn draw_box(
+    width: u16,
+    height: u16,
+    offset_x: u16,
+    offset_y: u16,
+    edge_symbols: EdgeSymbols,
+) -> Result<()> {
     let mut stdout = stdout();
 
-    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+    let height = height + offset_y;
+    let width = width + offset_x;
 
-    let mut border = HashMap::new();
-    border.insert("t", '─');
-    border.insert("b", '─');
-    border.insert("l", '|');
-    border.insert("r", '|');
-    border.insert("tl", '┌');
-    border.insert("tr", '┐');
-    border.insert("bl", '└');
-    border.insert("br", '┘');
-
-    let w = 150;
-    let h = 40;
-
-    for y in 0..h {
-        for x in 0..w {
+    for y in offset_y..height {
+        for x in offset_x..width {
             if x == 0 && y == 0 {
                 stdout
                     .queue(cursor::MoveTo(x, y))?
-                    .queue(style::Print(border["tl"]))?;
-            } else if x == 0 && y == h - 1 {
+                    .queue(style::Print(edge_symbols.tl))?;
+            } else if x == 0 && y == height - 1 {
                 stdout
                     .queue(cursor::MoveTo(x, y))?
-                    .queue(style::Print(border["bl"]))?;
-            } else if x == w - 1 && y == 0 {
+                    .queue(style::Print(edge_symbols.bl))?;
+            } else if x == width - 1 && y == 0 {
                 stdout
                     .queue(cursor::MoveTo(x, y))?
-                    .queue(style::Print(border["tr"]))?;
-            } else if x == w - 1 && y == h - 1 {
+                    .queue(style::Print(edge_symbols.tr))?;
+            } else if x == width - 1 && y == height - 1 {
                 stdout
                     .queue(cursor::MoveTo(x, y))?
-                    .queue(style::Print(border["br"]))?;
+                    .queue(style::Print(edge_symbols.br))?;
             } else {
                 if x == 0 {
                     stdout
                         .queue(cursor::MoveTo(x, y))?
-                        .queue(style::Print(border["l"]))?;
-                } else if x == w - 1 {
+                        .queue(style::Print(edge_symbols.l))?;
+                } else if x == width - 1 {
                     stdout
                         .queue(cursor::MoveTo(x, y))?
-                        .queue(style::Print(border["r"]))?;
+                        .queue(style::Print(edge_symbols.r))?;
                 } else if y == 0 {
                     stdout
                         .queue(cursor::MoveTo(x, y))?
-                        .queue(style::Print(border["t"]))?;
-                } else if y == h - 1 {
+                        .queue(style::Print(edge_symbols.t))?;
+                } else if y == height - 1 {
                     stdout
                         .queue(cursor::MoveTo(x, y))?
-                        .queue(style::Print(border["b"]))?;
+                        .queue(style::Print(edge_symbols.b))?;
                 }
             }
         }
     }
     stdout.flush()?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let mut stdout = stdout();
+
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+
+    let w = 150;
+    let h = 40;
+    let use_draw_box = false;
+
+    if use_draw_box {
+        _ = draw_box(w, h, 0, 0, EdgeSymbols::default());
+    } else {
+        let mut border = HashMap::new();
+        border.insert("t", '─');
+        border.insert("b", '─');
+        border.insert("l", '│');
+        border.insert("r", '│');
+        border.insert("tl", '┌');
+        border.insert("tr", '┐');
+        border.insert("bl", '└');
+        border.insert("br", '┘');
+
+        for y in 0..h {
+            for x in 0..w {
+                if x == 0 && y == 0 {
+                    stdout
+                        .queue(cursor::MoveTo(x, y))?
+                        .queue(style::Print(border["tl"]))?;
+                } else if x == 0 && y == h - 1 {
+                    stdout
+                        .queue(cursor::MoveTo(x, y))?
+                        .queue(style::Print(border["bl"]))?;
+                } else if x == w - 1 && y == 0 {
+                    stdout
+                        .queue(cursor::MoveTo(x, y))?
+                        .queue(style::Print(border["tr"]))?;
+                } else if x == w - 1 && y == h - 1 {
+                    stdout
+                        .queue(cursor::MoveTo(x, y))?
+                        .queue(style::Print(border["br"]))?;
+                } else {
+                    if x == 0 {
+                        stdout
+                            .queue(cursor::MoveTo(x, y))?
+                            .queue(style::Print(border["l"]))?;
+                    } else if x == w - 1 {
+                        stdout
+                            .queue(cursor::MoveTo(x, y))?
+                            .queue(style::Print(border["r"]))?;
+                    } else if y == 0 {
+                        stdout
+                            .queue(cursor::MoveTo(x, y))?
+                            .queue(style::Print(border["t"]))?;
+                    } else if y == h - 1 {
+                        stdout
+                            .queue(cursor::MoveTo(x, y))?
+                            .queue(style::Print(border["b"]))?;
+                    }
+                }
+            }
+        }
+        stdout.flush()?;
+    }
     Ok(())
 }
