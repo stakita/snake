ef691e0f47b9a284e2e7c525d2509ca06e498974
Simon Takita <smtakita@gmail.com>
2022-09-19 14:45:07 -0700

    Polish up boxing functionality

--- examples/ex3.rs
+++ examples/ex3.rs
@@ -1,22 +1,63 @@
-use crossterm::{
-    cursor,
-    style::{self, Stylize},
-    terminal, ExecutableCommand, QueueableCommand, Result,
+use crossterm::{cursor, style, terminal, ExecutableCommand, QueueableCommand, Result};
+use std::{
+    collections::HashMap,
+    io::{stdout, Write},
 };
-use std::io::{stdout, Write};
 
 fn main() -> Result<()> {
     let mut stdout = stdout();
 
     stdout.execute(terminal::Clear(terminal::ClearType::All))?;
 
-    for y in 0..40 {
-        for x in 0..150 {
-            if (y == 0 || y == 40 - 1) || (x == 0 || x == 150 - 1) {
-                // in this loop we are more efficient by not flushing the buffer.
+    let mut border = HashMap::new();
+    border.insert("t", '─');
+    border.insert("b", '─');
+    border.insert("l", '|');
+    border.insert("r", '|');
+    border.insert("tl", '┌');
+    border.insert("tr", '┐');
+    border.insert("bl", '└');
+    border.insert("br", '┘');
+
+    let w = 150;
+    let h = 40;
+
+    for y in 0..h {
+        for x in 0..w {
+            if x == 0 && y == 0 {
                 stdout
                     .queue(cursor::MoveTo(x, y))?
-                    .queue(style::PrintStyledContent("█".magenta()))?;
+                    .queue(style::Print(border["tl"]))?;
+            } else if x == 0 && y == h - 1 {
+                stdout
+                    .queue(cursor::MoveTo(x, y))?
+                    .queue(style::Print(border["bl"]))?;
+            } else if x == w - 1 && y == 0 {
+                stdout
+                    .queue(cursor::MoveTo(x, y))?
+                    .queue(style::Print(border["tr"]))?;
+            } else if x == w - 1 && y == h - 1 {
+                stdout
+                    .queue(cursor::MoveTo(x, y))?
+                    .queue(style::Print(border["br"]))?;
+            } else {
+                if x == 0 {
+                    stdout
+                        .queue(cursor::MoveTo(x, y))?
+                        .queue(style::Print(border["l"]))?;
+                } else if x == w - 1 {
+                    stdout
+                        .queue(cursor::MoveTo(x, y))?
+                        .queue(style::Print(border["r"]))?;
+                } else if y == 0 {
+                    stdout
+                        .queue(cursor::MoveTo(x, y))?
+                        .queue(style::Print(border["t"]))?;
+                } else if y == h - 1 {
+                    stdout
+                        .queue(cursor::MoveTo(x, y))?
+                        .queue(style::Print(border["b"]))?;
+                }
             }
         }
     }
