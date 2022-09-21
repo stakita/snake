26551837b4249caf418d1aeb3e0f982114776a30
Simon Takita <smtakita@gmail.com>
2022-09-21 14:35:06 -0700

    Remove redundant comments

--- src/snake/ui.rs
+++ src/snake/ui.rs
@@ -11,7 +11,6 @@ pub fn init(state: State) -> State {
     ncurses::cbreak();
     ncurses::noecho();
     ncurses::keypad(win, true);
-    ncurses::curs_set(ncurses::CURSOR_VISIBILITY::CURSOR_INVISIBLE); // This actually doesn't work for some reason
     hide_cursor();
 
     State {
@@ -28,6 +27,8 @@ pub fn fini(state: State) -> State {
 
 // TODO: handle error instead of suppressing
 fn hide_cursor() {
+    // ncurses::curs_set doesn't work for some reason
+    // Replacing with manual ansi escape code
     let mut stdout = stdout();
     let _ = stdout.write(&format!("{}", ansi_escapes::CursorHide).as_bytes().to_vec());
     let _ = stdout.flush();
@@ -49,14 +50,9 @@ pub fn game_over(state: State) -> State {
 }
 
 pub fn draw_screen(state: State) -> State {
-    //   def draw_screen(state) do
-    //     ExNcurses.clear()
     ncurses::clear();
-    //     ExNcurses.mvaddstr(0, 2, "Snake")
     ncurses::mvaddstr(0, 2, "Snake");
-    //     ExNcurses.wclear(state.game_win)
     ncurses::wclear(state.game_win);
-    //     ExNcurses.wborder(state.game_win)
     ncurses::wborder(
         state.game_win,
         '|' as u32,
@@ -71,38 +67,18 @@ pub fn draw_screen(state: State) -> State {
     let state = update_score(state);
     draw_snake(&state);
     draw_food(&state);
-    //     ExNcurses.refresh()
-
-    // We can't hide the cursor on macos, so just park it in the corner
-    // ncurses::wmove(state.game_win, 0, 0);
 
     ncurses::refresh();
-    //     ExNcurses.wrefresh(state.game_win)
     ncurses::wrefresh(state.game_win);
-    //     state
-    //   end
     state
 }
 
-//   defp draw_food(state) do
-//     {x, y} = state.food
-//     ExNcurses.wmove(state.game_win, y, x)
-//     ExNcurses.waddstr(state.game_win, "*")
-//     state
-//   end
 fn draw_food(state: &State) {
     let (x, y) = state.food.unwrap();
     ncurses::wmove(state.game_win, y, x);
     ncurses::waddstr(state.game_win, "*");
 }
 
-//   defp draw_snake(state, []), do: state
-
-//   defp draw_snake(state, [{x, y} | rest]) do
-//     ExNcurses.wmove(state.game_win, y, x)
-//     ExNcurses.waddstr(state.game_win, "#")
-//     draw_snake(state, rest)
-//   end
 fn draw_snake(state: &State) {
     let snake_iter = state.snake.iter();
     for elem in snake_iter {
@@ -110,7 +86,6 @@ fn draw_snake(state: &State) {
         ncurses::wmove(state.game_win, *y, *x);
         ncurses::waddstr(state.game_win, "#");
     }
-    // draw_snake(state, rest)
 }
 
 fn center_text(state: &State, text: &str) {
